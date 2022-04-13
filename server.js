59f7d6a91e8dbce46b1f5c8731b1f22ace72fbf6
Justin Hamerly <suvimountain@icloud.com>
2022-04-13 14:01:03 -0700

    added cors policy and can send messages

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const io = require('socket.io')(3000);
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const server = app.listen(3000);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 io.on('connection', socket => {
-  socket.emit('chat-message', 'Welcome!')
+  console.log('new User');
+  socket.emit('chat-message', 'Welcome!');
+  socket.on('send-chat-message', message => {
+    console.log(message)
+  })
 });
